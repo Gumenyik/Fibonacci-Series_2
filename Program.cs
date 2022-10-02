fda6cb1a13d8e88277ff53830b9305ba19c5009c
Gumenyik <78902763+Gumenyik@users.noreply.github.com>
2022-10-02 12:46:40 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+Console.Write("Кількість чисел: ");
+int k = Convert.ToInt32(Console.ReadLine());
+
+int a = 0, b = 1, c = 0;
+for(int i = 0; i < k; i++)
+{
+    c = a + b;
+    a = b;
+    b = c;
+    Console.WriteLine(c);
+}
+// change for commmit 
